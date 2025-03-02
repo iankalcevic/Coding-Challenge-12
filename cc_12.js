885903df5e4f6a268b6c0dab6161f120a4e07d22
iankalcevic <iankalcevic@usf.edu>
2025-03-02 14:50:03 -0500

    Task 4 - Demonstrated Event Bubbling in Customer Section

--- cc_12.js
+++ cc_12.js
@@ -53,3 +53,20 @@ document.getElementById("addProductButton").addEventListener("click", () => {
 //Example product items
 addProductItem("Milk");
 addProductItem("Paper");
+
+// Task 4 - Business Customer Section
+
+const customerSection = document.getElementById("customerSection");
+
+customerSelection.addEventListener("click", () => {
+    console.log("Customer Card Selected");
+}); //Add click event listener
+
+const customerCards = document.querySelectorAll ("customer-card");
+customerCards.forEach((card) => {
+    card.style.backgroundColor = "yellow"; //Set background color
+    card.addEventListener("click", (event) => {
+        console.log("Customer Card Selected");
+        event.stopPropagation();
+    });
+}); //Test cases
